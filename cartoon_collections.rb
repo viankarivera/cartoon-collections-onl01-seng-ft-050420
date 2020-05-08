6a6b2bd01754e67ebcac2e5237173baedafd0770
Vianka Rivera <viankalrivera@gmail.com>
2020-05-08 19:49:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(calls_long)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheddar_cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
