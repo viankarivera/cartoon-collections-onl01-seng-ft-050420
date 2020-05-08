0763985b22848c0c7a0e83f79cb9429c0b2e25dd
Vianka Rivera <viankalrivera@gmail.com>
2020-05-08 21:21:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(potentially_cheesy_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
   #if potentially_cheesy_items == 0
 
-  potentially_cheesy_items.find do |cheese|
+  potentially_cheesy_items.find do |maybe_cheese|
     cheese_types[1]
 end
 end
