f58a5648d6b8f9103c621de14aaa886cc0487d48
Vianka Rivera <viankalrivera@gmail.com>
2020-05-08 20:09:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,6 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(cheddar_cheese)
-  cheddar_cheese.find
+  cheddar_cheese.find |cheese|
   cheese_types.include?(cheddar_cheese)
 end
