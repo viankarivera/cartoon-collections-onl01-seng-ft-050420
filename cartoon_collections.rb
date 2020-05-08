dcf1897856a443fc71346c92739fcdb2648fd6e4
Vianka Rivera <viankalrivera@gmail.com>
2020-05-08 19:44:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def roll_call_dwarves(dwarfs)
     planeteer_calls
   end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  
 end
 
 def find_the_cheese# code an argument here
