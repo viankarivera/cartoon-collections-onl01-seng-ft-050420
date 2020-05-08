f60418bef9a6b0d3370e502712332d56b65a35b3
Vianka Rivera <viankalrivera@gmail.com>
2020-05-08 21:36:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+def roll_call_dwarves(dwarfs)
+  dwarfs.each_with_index do | name , index |
+      puts "#{index + 1} #{name}"
+    end
+
+  end
+
+  def summon_captain_planet(veggies)
+    planeteer_calls = []
+    veggies.collect do |calls|
+      planeteer_calls << "#{calls.capitalize}!"
+    end
+    planeteer_calls
+  end
+
+def long_planeteer_calls(calls_long)
+  calls_long.any? do |word|
+    word.length > 4
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+#potentially_cheesy_items = %w[banana cheddar sock]
+def find_the_cheese(potentially_cheesy_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheese_types.include? maybe_cheese
+end
 end
